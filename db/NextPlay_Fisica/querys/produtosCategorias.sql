9e36e29dd40e3005d91254c023ba0bb600d9e0d4
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2024-11-25 03:34:35 -0300

    Delete db directory

--- db/NextPlay_Fisica/querys/produtosCategorias.sql
+++
@@ -1,5 +0,0 @@
-/* Essa query traz os produtos e suas respectivas categorias*/
-SELECT P.NOME, C.CATEGORIA
-FROM PRODUTOS P 
-INNER JOIN CATEGORIAS C ON P.ID_CATEGORIA = C.ID_CATEGORIA
-GO
--- db/NextPlay_Fisica/querys/sb_ListarProdutos.sql
+++
@@ -1,5 +0,0 @@
-/* Listando produtos que estão com o preço acima da média */
-
-SELECT NOME, PRECO 
-FROM PRODUTOS 
-WHERE PRECO > (SELECT AVG(PRECO) FROM PRODUTOS)
